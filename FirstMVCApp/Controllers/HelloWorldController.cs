2af1750c3bb7ebe5b4aea1c98001a044fbf7cc6f
rpdudley <ryguy122000@gmail.com>
2023-08-30 16:16:19 -0400

    Addubg list to controoler then add it the view

--- FirstMVCApp/Controllers/HelloWorldController.cs
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -5,16 +5,30 @@ namespace FirstMVCApp.Controllers
 {
     public class HelloWorldController : Controller
     {
+        //add static for new element 
+        private static List<PersonViewModel> people = new List<PersonViewModel>();
         public IActionResult Index()
         {
-            DogViewModel doggo = new DogViewModel() 
-            { Name = "Sif", Age=2 };
-            return View(doggo);
+            //PersonViewModel person = new PersonViewModel() 
+          //{ Name = "Ryan", Age=22 };
+            return View(people);
 
         }
+        //This Iaction method returns the form using PersonViewModel it creates a new form everytime you hit submit 
         public IActionResult Create()
         {
-            return View();
+            var personVm = new PersonViewModel();
+            return View(personVm);
+
+        }
+        //Sends data to CreateForm 
+        //This Iaction gets called when you submit the form 
+        public IActionResult CreateForm(PersonViewModel personViewModel) 
+        {
+            
+            people.Add(personViewModel);
+            return RedirectToAction(nameof(Index));
+        
         }
         public string Hello()
         {
